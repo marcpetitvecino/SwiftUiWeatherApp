59d28844534d060c8d813e26453bac9055683327
marcpetitvecino <marcpetitvecino@gmail.com>
2022-08-10 12:43:16 +0200

    Initial Commit

---
+++ SwiftUiWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiWeatherApp
+//
+//  Created by Marc Petit Vecino on 10/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiWeatherApp/SwiftUiWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiWeatherAppApp.swift
+//  SwiftUiWeatherApp
+//
+//  Created by Marc Petit Vecino on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
